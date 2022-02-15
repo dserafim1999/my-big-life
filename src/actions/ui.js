883d7e1e4806e5516942ac3a4e995484f8e271d5
dserafim1999 <daniel.serafim@tecnico.ulisboa.pt>
2022-02-15 12:08:46 +0000

    added alert boxes

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -29,3 +29,18 @@ export const updateInternalBounds = (bounds) => {
     type: UPDATE_INTERNAL_BOUNDS
   }
 }
+
+export const addAlert = (message, type = 'error') => {
+  return {
+    message,
+    alertType: type,
+    type: ADD_ALERT
+  }
+}
+
+export const removeAlert = (alert) => {
+  return {
+    alert,
+    type: REMOVE_ALERT
+  }
+}
--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -1,12 +1,13 @@
-import { Map, fromJS } from 'immutable';
+import { fromJS } from 'immutable';
 import { 
   HIDE_TRACK_DETAILS,
+  REMOVE_ALERT,
   SHOW_TRACK_DETAILS,
   UPDATE_BOUNDS,
   UPDATE_INTERNAL_BOUNDS,
 } from '../actions';
 
-const initialState = Map({});
+const initialState = fromJS({ alerts: [] });
 
 const ui = (state = initialState, action) => {
     switch (action.type) {
@@ -18,6 +19,17 @@ const ui = (state = initialState, action) => {
         return state.set('details', false);
       case UPDATE_INTERNAL_BOUNDS:
         return state.set('internalBounds', action.bounds);
+      case REMOVE_ALERT:
+        return state.update('alerts', (alerts) => {
+          const index = alerts.findIndex((a) => a === action.alert);
+          if (index !== undefined) {
+            return alerts.delete(index);
+          } else {
+            return alerts;
+          }
+        });
+      case ADD_ALERT:
+        return state.update('alerts', (alerts) => alerts.push({ type: action.alertType, message: action.message }));
       default:
         return state;
     }
