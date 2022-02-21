fe25fdb7f081b76839a690f69fde80aa396f5957
dserafim1999 <daniel.serafim@tecnico.ulisboa.pt>
2022-02-21 10:40:53 +0000

    order tracks

--- src/containers/TrackList/index.js
+++ src/containers/TrackList/index.js
@@ -5,16 +5,28 @@ import Card from "../Card";
 import TrackInfo from './TrackInfo';
 
 let TrackList = ({ dispatch, tracks, segments, className }) => {
-    return (
+    const findStart = (seg) =>
+        seg.get('segments').map((s) => segments.get(s).get('start')).sort((_a, _b) => _a.get('start').diff(_b.get('start'))).get(0);
+    
+    if (tracks.count() !== 0) {
+        return (
             <ul style={{padding: 0}}>
                 {
-                    tracks.valueSeq().map((track, i) => {
+                    tracks.valueSeq().sort((a, b) => {
+                        const aStart = findStart(a)
+                        const bStart = findStart(b)
+                        return aStart.get('start').diff(bStart.get('start'))
+                      })
+                      .map((track, i) => {
                         const trackSegments = track.get('segments').map((id) => segments.get(id));
                         return <TrackInfo dispatch={dispatch} track={track} segments={trackSegments} key={i} />
                       })
                 }
             </ul>
-    )
+        )
+    }
+
+    return null;
 } 
 
 const mapStateToProps = (state) => {
