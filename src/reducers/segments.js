752a33ab2d52939e5762e7a9a66a1d58ff39d0dc
dserafim1999 <daniel.serafim@tecnico.ulisboa.pt>
2022-02-24 14:48:11 +0000

    extend one point segments

--- src/reducers/segments.js
+++ src/reducers/segments.js
@@ -69,10 +69,12 @@ const extendSegment = (state, action) => {
         }
     }
 
+    const points = state.get('segments').get(id).get('points');
+
     let point = fromJS({
       lat: action.lat,
       lon: action.lon,
-      time: extrapolateTime(state.get('segments').get(id).get('points'), action.index)
+      time: points.count() === 1 ? points.get(0).get('time').clone().add(1000) : extrapolateTime(points, action.index)
     });
  
     return state.updateIn(['segments', id, 'points'], (points) => {
