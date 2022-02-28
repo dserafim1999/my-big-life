b6d53a5b41ca3e999473d7fda4cfdd175b8a09cb
dserafim1999 <daniel.serafim@tecnico.ulisboa.pt>
2022-02-28 10:45:20 +0000

    improve config pane

--- src/containers/ConfigPane/index.js
+++ src/containers/ConfigPane/index.js
@@ -1,145 +1,82 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { 
-  saveConfig, 
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  saveConfig,
   loadCanonicalTrips,
   loadCanonicalLocations
-} from '../../actions/progress'
-import { toggleConfig } from '../../actions/ui'
-import AsyncButton from '../../components/AsyncButton'
+} from '../../actions/progress';
+import { toggleConfig } from '../../actions/ui';
+import AsyncButton from '../../components/AsyncButton';
+import { TextField, ToggleField, OptionsField, SectionBlock } from '../../components/Form';
 
-class TextField extends Component {
-  getValue () {
-    if (this.props.type === 'number') {
-      return parseFloat(this.refs.field.value)
-    } else {
-      return this.refs.field.value
-    }
-  }
-
-  render () {
-    const { title, placeholder, type, defaultValue, help, ...details } = this.props
-    return (
-      <span key={title}>
-        <label className='label'> {title} </label>
-        <p className='control has-addons' style={{ marginBottom: '0' }}>
-          <input {...details} className='input is-expanded' type={type || 'text'} defaultValue={defaultValue} placeholder={placeholder} ref='field' />
-        </p>
-        <blockquote className='help' style={{ color: 'gray'/* , fontSize: '0.9rem' */, marginBottom: '10px' }}>
-          {help}
-        </blockquote>
-      </span>
-    )
-  }
-}
-
-class ToggleField extends Component {
-  getValue () {
-    return this.refs.field.getValue()
-  }
-
-  render () {
-    const { title, checked } = this.props
-    return (
-      <OptionsField title={title} options={[{label: 'Yes', key: true}, {label: 'No', key: false}]} selected={checked} ref='field' />
-    )
-  }
-}
-
-class OptionsField extends Component {
-  getValue () {
-    return this.refs.field.value
-  }
-
-  render () {
-    const { title, options, selected, help } = this.props
-    return (
-      <span key={title}>
-        <label className='label'> {title} </label>
-        <p className='control'>
-          <span className='select'>
-            <select defaultValue={selected} ref='field'>
-              { options.map((option) => (<option key={option.key} value={option.key} >{option.label}</option>)) }
-            </select>
-          </span>
-        </p>
-        <blockquote className='help' style={{ color: 'gray', /* fontSize: '0.9rem', */ marginBottom: '10px' }}>
-          {help}
-        </blockquote>
-      </span>
-    )
-  }
-}
-
-const SectionBlock = ({ name, children }) => {
-  const blockStyle = {
-    marginTop: '10px'
-  }
-  const blockHeader = {
-    fontSize: '1.4rem'
-  }
-  return (
-    <div style={{ marginTop: '20px' }}>
-      <h1 style={blockHeader}> {name} </h1>
-      <div style={blockStyle}>
-        {children}
-      </div>
-    </div>
-  )
+const style = {
+  position: 'absolute',
+  top: '10%',
+  left: '50%',
+  width: '50%',
+  height: '80%',
+  marginLeft: '-25%',
+  zIndex: 700,
+  backgroundColor: 'white',
+  borderRadius: '8px',
+  padding: '5px',
+  display: 'flex',
+  flexDirection: 'column'
 }
 
 class ConfigPane extends Component {
   render () {
-    const { dispatch, address, config } = this.props
-    const style = {
-      position: 'absolute',
-      top: '10%',
-      left: '50%',
-      width: '50%',
-      height: '80%',
-      marginLeft: '-25%',
-      zIndex: 700,
-      backgroundColor: 'white',
-      borderRadius: '8px',
-      padding: '5px',
-      display: 'flex',
-      flexDirection: 'column'
-    }
+    const { dispatch, address, config } = this.props;
 
     const onSubmit = (e) => {
-      //e.preventDefault()
-      const rr = {}
+      // e.preventDefault()
+      const rr = {};
       Object.keys(this.refs).forEach((key) => {
-        const value = this.refs[key].getValue()
-        const k = key.split('.')
+        const value = this.refs[key].getValue();
+        
+        const k = key.split('.');
         switch (k.length) {
           case 1:
-            rr[k[0]] = value
-            break
+            rr[k[0]] = value;
+            break;
           case 2:
             if (!rr[k[0]]) {
-              rr[k[0]] = {}
+              rr[k[0]] = {};
             }
-            rr[k[0]][k[1]] = value
-            break
+            rr[k[0]][k[1]] = value;
+            break;
           case 3:
             if (!rr[k[0]]) {
-              rr[k[0]] = {}
+              rr[k[0]] = {};
             }
             if (!rr[k[0]][k[1]]) {
-              rr[k[0]][k[1]] = {}
+              rr[k[0]][k[1]] = {};
             }
-            rr[k[0]][k[1]][k[2]] = value
-            break
+            rr[k[0]][k[1]][k[2]] = value;
+            break;
         }
-      })
-      dispatch(saveConfig(rr))
+      });
+      dispatch(saveConfig(rr));
     }
 
-    let serverSpecific = <div></div>
+    let serverSpecific = <div></div>;
     if (config) {
+      let timezones = [];
+      for (let tz = -12; tz < 15; tz++) {
+        timezones.push(
+          {
+            label: 'UTC' + (tz >= 0 ? '+' + tz : tz),
+            key: tz
+          }
+        );
+      }
+
       serverSpecific = (
         <div>
+          <SectionBlock name='General'>
+            <OptionsField title='Default timezone' options={timezones} defaultValue={config.default_timezone} ref='default_timezone' />
+          </SectionBlock>
+
           <SectionBlock name='Folders'>
             <TextField title='Input folder' defaultValue={config.input_path} ref='input_path' help='Path to the folder containing tracks to be processed' />
             <TextField title='Destination folder' defaultValue={config.output_path} ref='output_path' help='Path to folder where the processed tracks wil be saved' />
@@ -162,6 +99,7 @@ class ConfigPane extends Component {
           </SectionBlock>
 
           <SectionBlock name='Smoothing'>
+            <ToggleField title='Use' checked={config.smoothing.use} ref='smoothing.use' />
             <OptionsField title='Algorithm' options={[{ label: 'Kalman with backwards pass', key: 'inverse' }, { label: 'Kalman with start interpolation', key: '' }]} defaultValue={config.smoothing.algorithm} ref='smoothing.algorithm' help={
               <span>
                 Algorithm to use to smooth tracks. There are two possibilities:
@@ -175,11 +113,13 @@ class ConfigPane extends Component {
           </SectionBlock>
 
           <SectionBlock name='Spatiotemporal segmetation'>
+            <ToggleField title='Use' checked={config.segmentation.use} ref='segmentation.use' />
             <TextField title='Epsilon' defaultValue={config.segmentation.epsilon} ref='segmentation.epsilon' type='number' min='0' step='0.01' help='Distance epsilon after which points can be clustered into the same stop. Points are clustered based on their spatiotemporal distance. The higher it is the less clusters will exist.'/>
             <TextField title='Min. time' defaultValue={config.segmentation.min_time} ref='segmentation.min_time' type='number' min='0' step='1' help='Minimum time at one place to consider it a stop' />
           </SectionBlock>
 
           <SectionBlock name='Simplification'>
+            <ToggleField title='Use' checked={config.simplification.use} ref='simplification.use' />
             <TextField title='Max. distance error' defaultValue={config.simplification.max_dist_error} ref='simplification.max_dist_error' type='number' min='0' step='0.5' help='Maximum distance error, in meters. Higher values give higher compression rates but also more deviations from the original track' />
             <TextField title='Max. speed error' defaultValue={config.simplification.max_speed_error} ref='simplification.max_speed_error' type='number' min='0' step='0.5' help='Maximum speed error, in km/h. Higher values give higher compression rates but also more deviations from the original track' />
             <TextField title='Epsilon' defaultValue={config.simplification.eps} ref='simplification.eps' type='number' min='0' step='0.1' help='Maximum distance, in degrees, to compress a track solely based on its topology' />
@@ -205,22 +145,24 @@ class ConfigPane extends Component {
             <TextField title='Epsilon' defaultValue={config.trip_learning.epsilon} ref='trip_learning.epsilon' type='number' min='0' step='0.01' />
           </SectionBlock>
         </div>
-      )
+      );
     }
 
     return (
       <div style={style}>
         <header style={{ fontSize: '2rem' }}>
           Configuration
-          <a className='button is-small' onClick={() => dispatch(loadCanonicalTrips())}>Load canonical trips</a>
-          <a className='button is-small' onClick={() => dispatch(loadCanonicalLocations())}>Load canonical locations</a>
+          <a className='button is-small' style={{ float: 'right' }} onClick={() => dispatch(loadCanonicalTrips())}>Load canonical trips</a>
+          <a className='button is-small' style={{ float: 'right' }} onClick={() => dispatch(loadCanonicalLocations())}>Load canonical locations</a>
         </header>
         <section style={{ flexGrow: 1, overflowY: 'auto' }}>
           <div style={{ maxWidth: '400px', margin: 'auto' }}>
             <SectionBlock name='Server Address'>
               <TextField title='' defaultValue={address} ref='_.server' />
             </SectionBlock>
+
             { serverSpecific }
+
           </div>
         </section>
         <footer style={{ textAlign: 'right' }} className='control'>
@@ -235,7 +177,7 @@ class ConfigPane extends Component {
           }} > Save </AsyncButton>
         </footer>
       </div>
-    )
+    );
   }
 }
 
@@ -244,9 +186,9 @@ const mapStateToProps = (state) => {
   return {
     address: state.get('progress').get('server'),
     config: serverConfig ? serverConfig.toJS() : null
-  }
+  };
 }
 
-const CPane = connect(mapStateToProps)(ConfigPane)
+const CPane = connect(mapStateToProps)(ConfigPane);
 
-export default CPane
+export default CPane;
