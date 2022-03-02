7bb72af7dbfde710d82cc2234db3dae4fcc5c9b0
dserafim1999 <daniel.serafim@tecnico.ulisboa.pt>
2022-03-02 14:08:41 +0000

    refactored track processing to route

--- src/containers/MainContainer/index.js
+++ src/containers/MainContainer/index.js
@@ -2,21 +2,16 @@ import React from "react";
 
 import LeafletMap from "../LeafletMap";
 
-import SideBar from "../../components/SideBar";
-
 import ConfigPane from '../ConfigPane';
 
 import AlertBox from "../AlertBox";
-import SidePane from "../SidePane";
+import SidePane from "../TrackProcessing";
 
 let MainContainer = ({ onKeyUp, onKeyDown, showConfig, step }) => {
   return (
         <div id='container' onKeyUp={onKeyUp} onKeyDown={onKeyDown} > 
           <AlertBox/>
-          {
-          //<SideBar/>}
-          }
-          <SidePane/>
+          {/*<SidePane/>*/}
           <LeafletMap/>
           { showConfig ? <ConfigPane /> : null }
         </div>
