7f2c4a6cdbae4b72c7b8ac8c2f2fcbb0127579b6
Daniel Serafim <48388827+dserafim1999@users.noreply.github.com>
2022-06-30 17:27:08 +0100

    fixed inconsistencie in Search

--- src/modules/Search/SearchRoute.js
+++ src/modules/Search/SearchRoute.js
@@ -21,8 +21,8 @@ const SearchRoute = ({id, queryState, start, end, dispatch}) => {
         <SectionBlock name={'Route ('+start+'→'+end+')'}>
             <TextField title='Route' onChange={(e) => onChange({route: e.target.value})}/>
             <TextField title='Duration' hasOperators={true} onChange={(e) => onChange({duration: addSuffix(e.target.value, 'min')})} type='number' min={0} suffix={"min"}/>
-            <TextField title='Start Range' hasOperators={true} onChange={(e) => onChange({temporalStartRange: addSuffix(e.target.value, 'min')})} type='number' min={0} suffix={"min"}/>
-            <TextField title='End Range' hasOperators={true} onChange={(e) => onChange({temporalEndRange: addSuffix(e.target.value, 'min')})} type='number' min={0} suffix={"min"}/>
+            <TextField title='Start Range' onChange={(e) => onChange({temporalStartRange: addSuffix(e.target.value, 'min')})} type='number' min={0} suffix={"min"}/>
+            <TextField title='End Range' onChange={(e) => onChange({temporalEndRange: addSuffix(e.target.value, 'min')})} type='number' min={0} suffix={"min"}/>
         </SectionBlock>
     );
 };
