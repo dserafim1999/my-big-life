66e4bbe9cba6d1135312c19826a05047866b8659
Daniel Serafim <48388827+dserafim1999@users.noreply.github.com>
2022-09-02 16:07:47 +0100

    setup styleguidist for documentation

---
+++ styleguide.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    title: 'My Big Life',
+    sections: [
+      {
+        name: 'Introduction',
+        content: 'docs/introduction.md'
+      },
+      {
+        name: 'Documentation',
+        sections: [
+          {
+            name: 'Installation',
+            content: 'docs/installation.md',
+            description: 'The description for the installation section'
+          },
+          {
+            name: 'Configuration',
+            content: 'docs/configuration.md'
+          }
+        ]
+      }
+    ],
+    webpackConfig: {
+      module: {
+        rules: [
+          {
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          }
+        ]
+      }
+    }
+  }
