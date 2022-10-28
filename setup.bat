089c611e48bd60687d9a01268c5419ff4eb2d741
Daniel Serafim <48388827+dserafim1999@users.noreply.github.com>
2022-10-28 11:52:32 +0100

    added default directories and config file to setup

--- setup.bat
+++ setup.bat
@@ -6,7 +6,7 @@ CALL npm install
 ECHO Front-End Setup Complete!
 
 CALL cd ../backend && python -m venv venv 
-CALL .\venv\Scripts\Activate & pip install -r "requirements.txt"
+CALL .\venv\Scripts\Activate & pip install -r "requirements.txt" && python .\setup_files.py
 
 ECHO Back-End Setup Complete!
 
